7b9bf29cd5a7c39f4f13e1a1a601f8e87d08fe2e
hquan08 <quanlehoang2508@gmail.com>
2021-07-27 18:01:05 +0700

    Add files via upload

---
+++ discordbot.js
@@ -0,0 +1,133 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fetch = require('node-fetch');
+const translator = require('@iamtraction/google-translate');
+const weather = require('weather-js');
+
+
+const memeSubreddit = ['dankmemes', 'memes', 'Animemes']; //array contains some subreddit
+
+const cmd = "!";
+
+
+function randomSubreddit(num) {
+    return Math.floor(Math.random() * num);
+}
+
+client.on("ready", () => {
+    console.log('the bot is running');
+});
+
+
+client.on("message", async (msg) => {
+    if (msg.author.bot)
+        return;
+    //an interaction with the bot
+    if (msg.content === "Hello bot".toLowerCase() || msg.content === "Hi bot".toLowerCase()) {
+        msg.reply("Hello there");
+    }
+    //another interaction with the bot
+    if (msg.content === "Stupid bot".toLowerCase()) {
+        msg.channel.send("Did your mum not teach you to be a kind person?");
+    }
+    //yet another interaction with the bot
+    if (msg.content === "Handsome bot".toLowerCase()) {
+        msg.react('❤️');
+        msg.reply("You're damn right!");
+    }
+
+    //command to return a joke
+    if (msg.content.startsWith(cmd + "joke")) {
+        let getData = async () => {
+            let resp = await fetch("https://official-joke-api.appspot.com/random_joke");
+            let json = resp.json();
+            return json;
+        }
+        let joke = await getData();
+
+        //I use translator.js library
+        translator(`Random joke #${joke.id}:\n ${joke.setup} - ${joke.punchline}`, { default: "en" })
+            //don't work great for Vietnamese language
+            //simply return like msg.reply(`Random joke #${joke.id}:\n ${joke.setup} - ${joke.punchline}`) if you don't need a translator
+            .then(res => {
+                msg.reply(res.text);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    //find weather of a city
+    if (msg.content.startsWith(cmd + "weather")) {
+        let message = msg.content.toLowerCase();
+        let query = msg.content.slice(cmd.length).split(" "); //cut "!", then put the rest into array - in this case, 'query' variable
+        let city = query.slice(1).join(" "); //then we slice 'weather' and join the rest to become a string (it means location name)
+        weather.find({ search: city, degreeType: 'C' }, async (err, res) => {
+            if (err) {
+                msg.reply("Please input the name of location you want to find");
+            }
+            else {
+                if (res[0] !== undefined) {
+                    var loc = res[0].location;
+                    var cur = res[0].current;
+                    const embedMessage = new Discord.MessageEmbed()
+                        .setTitle('Thời tiết tại ' + "" + city)
+                        .setThumbnail(cur.imageUrl)
+                        .setTimestamp()
+                        .addField("Địa điểm", `${loc.name}`)
+                        .addFields(
+                            { name: "Múi giờ", value: loc.timezone, inline: true },
+                            { name: "Nhiệt độ", value: cur.temperature + " độ C", inline: true },
+                            { name: "Ngày hiện tại", value: cur.date, inline: true }
+                        )
+                        .addFields(
+                            { name: "Quang cảnh", value: cur.skytext, inline: true },
+                            { name: "Độ ẩm", value: cur.humidity + "%", inline: true },
+                            { name: "Tốc độ gió", value: cur.windspeed, inline: true }
+                        )
+                        .setFooter('Data collected via weather.service.msn.com');
+                    await msg.reply(embedMessage);
+                }
+                else {
+                    await msg.reply(`There is no location **${city}**, you idiot!!`);
+                }
+            }
+        });
+    }
+
+    //get memes
+    if (msg.content.startsWith(cmd + "getmeme")) {
+        let index = randomSubreddit(memeSubreddit.length); //get random index of the array
+        let endpoint = memeSubreddit[index]; //attach it to the 'endpoint' variable
+        let getMeme = async () => {
+            let tmp;
+            await fetch(`https://meme-api.herokuapp.com/gimme/${endpoint}`)
+                .then(resp => resp.json())
+                .then(body => {
+                    tmp = body;
+                });
+            return tmp;
+        }
+        let meme = await getMeme();
+        const messageEmbed = new Discord.MessageEmbed()
+            .setTitle(meme.title)
+            .setImage(meme.url)
+            .setAuthor(meme.author)
+            .setURL(meme.postlink)
+            .setFooter(meme.ups + " upvotes")
+            .setTimestamp();
+        msg.reply(messageEmbed);
+    }
+
+
+    //see available commands
+    if (msg.content.startsWith(cmd + "command")) {
+        msg.channel.send("Available commands: !joke, !weather, !getmeme");
+    }
+});
+
+
+
+client.login(process.env.TOKEN);
